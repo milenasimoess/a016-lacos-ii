9ff13e5d42ef05c8be28c540fdffd78120b5fb91
milenarismo <milenasimoeslabenu@gmail.com>
2022-09-13 15:54:05 -0300

    Finalizado

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -5,3 +5,12 @@ const array = [
     [20, 33, 11, 12, 19],
     [3, 3, 4, 5, 10]
 ]
+
+for(let i = 0; i< array.length; i++){
+    let frase = `Jogador ${i+1}:`
+
+    for(let j in array[i]){
+        frase += array[i][j] + ","
+    }
+
+console.log(frase)}
---
+++ exercicio2/script.js
@@ -0,0 +1,8 @@
+const num = Number(prompt("Insira um número"))
+let arrayResultados = []
+
+for (let i = 0; i <= 10; i++) {
+    arrayResultados = num*[i]
+    console.log(arrayResultados)
+   
+}
